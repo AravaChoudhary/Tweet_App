5ddc05dd8a7be022b57f51e05e26a3400623b7cd
Arava Choudhary <aravachoudhary11@gmail.com>
2024-08-14 00:39:13 +0530

    Trying to Host

--- tweetapp/settings.py
+++ tweetapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^hf^)z$es-%d))3dc$kr4uveow75k_n+s^3n0zq$haq-)jyubf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh',]
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -128,7 +128,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATIC_URL= '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_DIRS = [os.path.join(BASE_DIR,'accounts/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/tweet/'
